d56be577074f4619d6f4a910746f3da85553baad
CodingMayus <106496765+CodingMayus@users.noreply.github.com>
2023-12-02 19:52:22 -0500

    Update background.js

--- background.js
+++ background.js
@@ -3,58 +3,47 @@
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({'bits':0});
-  chrome.storage.sync.set({power:true})
+  chrome.storage.sync.set({"bits":0});
+  chrome.storage.sync.set({"power":true})
   chrome.storage.sync.set({color:"#00ff00"});
- 
-  
-
-
-  chrome.storage.sync.set({ inputKeys:[ ]});
-
-
-
+  chrome.storage.sync.set({ "inputKeys":[{name:"Youtube",url:"www.youtube.com"},
+  {name:"Facebook",url:"www.facebook.com"},
+{name:"Cool Math Games",url:"www.coolmathgames.com"},
+{name:"Discord",url:"www.discord.com"},
+{name:"Instagram",url:"www.instagram.com"}]});
   //chrome.storage.sync.set({ color });
-  chrome.tabs.create({'url': "https://docs.google.com/document/d/1Vo4LuZFXS5vKjQNyIixMGPHOQB7OpJDw8UMvmYdiwmI/edit?usp=sharing"});
+  //chrome.tabs.create({'url': "https://docs.google.com/document/d/1Vo4LuZFXS5vKjQNyIixMGPHOQB7OpJDw8UMvmYdiwmI/edit?usp=sharing"});
  // console.log('Default background color set to %cgreen', `color: ${color}`);
+ 
+
+
 })
 
-//// background.js ////
-
-// Where we will expose all the data we retrieve from storage.sync.
-const storageCache = {};
-// Asynchronously retrieve data from storage.sync, then cache it.
-const initStorageCache = getAllStorageSyncData().then(items => {
-  // Copy the data retrieved from storage into storageCache.
-  Object.assign(storageCache, items);
-});
 
 
-chrome.action.onClicked.addListener(async (tab) => {
-  try {
-    await initStorageCache;
-  } catch (e) {
-    // Handle error that occurred during storage initialization.
-    console.log('wrong');
-  }
-  // Normal action handler logic.
-});
-
-// Reads all data out of storage.sync and exposes it via a promise.
-//
-// Note: Once the Storage API gains promise support, this function
-// can be greatly simplified.
-function getAllStorageSyncData() {
-  // Immediately return a promise and start asynchronous work
-  return new Promise((resolve, reject) => {
-    // Asynchronously fetch all data from storage.sync.
-    chrome.storage.sync.get(null, (items) => {
-      // Pass any observed errors down the promise chain.
-      if (chrome.runtime.lastError) {
-        return reject(chrome.runtime.lastError);
-      }
-      // Pass the data retrieved from storage down the promise chain.
-      resolve(items);
-    });
+// setInterval(function(){
+  chrome.storage.onChanged.addListener(function(changes, areaName) {
+    // Check if 'power' is present in the changes object
+    if ("power" in changes) {
+      chrome.storage.sync.get("power", function(data) {
+        if (data.power) {
+          chrome.action.setBadgeText({
+            text: "ON"
+          });
+        } else {
+          chrome.action.setBadgeText({
+            text: "OFF"
+          });
+        }
+      });
+    }
   });
-}
+
+  setInterval(function(){
+    chrome.storage.sync.get("bits",function(data){
+      var total = data.bits;
+      total++;
+    chrome.storage.sync.set({"bits":total});
+    })
+  },60000)  
+// },100)
